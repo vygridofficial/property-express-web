a82ce47f5cd46329ed140da7a6478d2645a1b8ca
ziuus <noelyt101@gmail.com>
2026-05-19 17:03:42 +0530

    Fix MOU PDF overlay positioning for page 3 and page 4 fields

--- src/utils/generateAgreementPDF.js
+++ src/utils/generateAgreementPDF.js
@@ -195,17 +195,17 @@ const generateMouAgreementPDF = async (submissionData, adminSignatureData, downl
   else drawCheckbox(doc, 111, 270);
 
   doc.setPage(3);
-  drawFitText(doc, price, 12, 57, 75, { fontSize: 7.5 });
-  drawFitText(doc, duration, 111, 57, 75, { fontSize: 7.5 });
+  drawFitText(doc, price, 12, 64, 75, { fontSize: 7.5 });
+  drawFitText(doc, duration, 111, 64, 75, { fontSize: 7.5 });
   drawFitText(doc, tokenAdvance, 76, 90, 80);
   drawFitText(doc, validUntil, 56, 226, 74);
 
   doc.setPage(4);
   drawFitText(doc, sellerName, 32, 130, 70);
-  drawDateSlots(doc, date, 40, 160);
+  drawDateSlots(doc, date, 40, 169);
   drawFitText(doc, authorizedSignatory, 80, 175, 58);
   drawFitText(doc, designation, 64, 191, 70);
-  drawDateSlots(doc, date, 30, 229);
+  drawDateSlots(doc, date, 30, 234);
 
   if (signatures.seller) {
     doc.addImage(signatures.seller, 'PNG', 32, 136, 38, 12);
